25d388405244d6d6d465a7f0f75f40097f9dde67
1161303225@student.mmu.edu.my <1161303225@student.mmu.edu.my>
2020-02-13 20:03:49 +0800

    REARRANGED FOLDERS

--- DeliveryOrder/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from DeliveryOrder.models import DeliveryOrder,DeliveryOrderItem
-
-admin.site.register(DeliveryOrder)
-admin.site.register(DeliveryOrderItem)
--- app/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from app.models import Person,Item,Vendor
-
-
-admin.site.register(Person)
-admin.site.register(Vendor)
-admin.site.register(Item)
